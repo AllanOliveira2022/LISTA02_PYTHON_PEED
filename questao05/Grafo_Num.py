429093620914162b9eee0770dcffa3e4a7206673
AllanOliveira2022 <115299780+AllanOliveira2022@users.noreply.github.com>
2023-04-24 20:41:30 -0300

    RESPOSTAS 'GRAFO'
    Respostas das questões '5,10,15,20'

---
+++ questao05/Grafo_Num.py
@@ -0,0 +1,19 @@
+def main():
+    grafo = {}
+    vertices = input('Digite os vértices do grafo separados por espaço: ').split()
+
+    for chave in vertices:
+        grafo[chave] = []
+    num_aresta = int(input("Quantas arestas vai ter o grafo: "))
+    for i in range(0,num_aresta):
+        partida, chegada = input('Digite os vértices da aresta separados por espaço : ').split()
+       
+        grafo[partida].append(chegada)
+        grafo[chegada].append(partida)
+
+
+    print("Grafo montado: ")
+    for v in grafo:
+        print(v, '->', grafo[v])
+if __name__ == "__main__":
+    main()
---
+++ questao10/Grafo_Remover.py
@@ -0,0 +1,31 @@
+def main():
+    grafo = {}
+    vertices = input('Digite os vértices do grafo separados por espaço: ').split()
+
+    for chave in vertices:
+        grafo[chave] = []
+    num_aresta = int(input("Quantas arestas vai ter o grafo: "))
+    for i in range(0,num_aresta):
+        partida, chegada = input('Digite os vértices da aresta separados por espaço : ').split()
+        
+        grafo[partida].append(chegada)
+        grafo[chegada].append(partida)
+
+
+
+    print("Grafo montado: ")
+    for v in grafo:
+        print(v, '->', grafo[v])
+        
+    print("Agora vamos apagar uma das arestas: ")
+    P_apagar, C_apagar = input("Digite os pares de arestas para apagar: ").split()  
+    if P_apagar in grafo[partida] and C_apagar in grafo[chegada]:
+        grafo[P_apagar].remove(C_apagar)
+        grafo[C_apagar].remove(P_apagar)
+        
+    print("Novo Grafo montado: ")
+    for v in grafo:
+        print(v, '->', grafo[v])        
+    
+if __name__ == "__main__":
+    main()
